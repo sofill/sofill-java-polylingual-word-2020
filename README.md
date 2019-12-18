ed61d356144e60c6e90bc4fcdb49ce2dd434d4b7
Sophia Lee <sofill.lee@gmail.com>
2019-12-18 16:03:42 +0900

    lets start!

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sofill-poly-lang-manager
-회원제 다개국어 단어암기 매니져
+회원제 다개국어 단어암기 매니저
---
+++ src/main/java/com/sofillpolylangmanager/App.java
@@ -0,0 +1,11 @@
+//프로젝트 : ㅇㅇㅇㅇㅇ
+
+package com.sofillpolylangmanager;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    System.out.println("Sofill의 단어암기시스템에 오신 것을 환영합니다.");
+  }
+}

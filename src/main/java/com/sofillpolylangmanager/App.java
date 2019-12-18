ddd93e65fc6021f7fb03fb69aad8b167a4948a72
Sophia Lee <sofill.lee@gmail.com>
2019-12-18 16:26:20 +0900

    222 출력

--- src/main/java/com/sofillpolylangmanager/App.java
+++ src/main/java/com/sofillpolylangmanager/App.java
@@ -1,4 +1,4 @@
-//프로젝트 : ㅇㅇㅇㅇㅇ
+//프로젝트 : 회원제 다개국어 단어암기 시스템
 
 package com.sofillpolylangmanager;
 
@@ -6,6 +6,8 @@ public class App {
 
   public static void main(String[] args) {
 
-    System.out.println("Sofill의 단어암기시스템에 오신 것을 환영합니다.");
+    System.out.println("Sofill의 단어암기장에 오신 것을 환영합니다.");
+    System.out.println("111");
+    System.out.println("222");
   }
 }
